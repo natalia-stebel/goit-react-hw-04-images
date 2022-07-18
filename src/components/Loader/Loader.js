5ec65c962181646a30128c007262c4b7e21ee89d
natalia-stebel <93938115+natalia-stebel@users.noreply.github.com>
2022-07-18 22:15:13 +0300

    add loader

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import {Bars} from 'react-loader-spinner';
+import { Bars } from 'react-loader-spinner';
 import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
+import css from './loader.module.css';
 
-export const Spinner =() => {
-  return <Bars color="#00BFFF" height={80} width={80} />;
-}
-
+export const Spinner = () => {
+  return <Bars className={css.loader} color="#00BFFF" height={90} width={90} />;
+};
